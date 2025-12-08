07fa8e6c032a566650c480bce9ea6ce6b3b19e73
Kelly Luo <kkelly.luo@gmail.com>
2025-12-08 14:18:58 +0100

    ajout des fonctions shell dutycycle pour controler directement le rapport cyclique et motorStart et motorStop

--- TP/software/Base/Core/Inc/motor_control/motor.h
+++ TP/software/Base/Core/Inc/motor_control/motor.h
@@ -12,7 +12,11 @@
 
 void motor_init(void);
 
-int motor_control(h_shell_t* h_shell, int argc, char** argv);
+int motor_speed(h_shell_t* h_shell, int argc, char** argv);
+int motor_dutycycle(h_shell_t* h_shell, int argc, char** argv);
+
+int motor_start(h_shell_t* h_shell, int argc, char** argv);
+int motor_stop(h_shell_t* h_shell, int argc, char** argv);
 
 
 #endif /* INC_MOTOR_CONTROL_MOTOR_H_ */
--- TP/software/Base/Core/Src/motor_control/motor.c
+++ TP/software/Base/Core/Src/motor_control/motor.c
@@ -13,7 +13,7 @@
 
 #define MOTOR_SPEED_MAX 3000
 
-float dutyCycle = 0.6f;
+float dutyCycle = 0.0f;
 
 void motor_init(void)
 {
@@ -23,17 +23,15 @@ void motor_init(void)
 	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, (uint32_t)(dutyCycle * PWM_ARR));
 	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, (uint32_t)((1 - dutyCycle) * PWM_ARR));
 
-	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
-	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
 
-	HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_1);
-	HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_2);
-
-	shell_add(&hshell1, "speed", motor_control, "Control motor speed (tr/min)");
+	shell_add(&hshell1, "speed", motor_speed, "Control motor speed (tr/min)");
+	shell_add(&hshell1, "dutycycle", motor_dutycycle, "Control duty cycle (0-100)");
+	shell_add(&hshell1, "motorStart", motor_start, "Start les PWM");
+	shell_add(&hshell1, "motorStop", motor_stop, "Stop les PWM");
 }
 
 
-int motor_control(h_shell_t* h_shell, int argc, char** argv)
+int motor_speed(h_shell_t* h_shell, int argc, char** argv)
 {
 	int size;
 	int speedValue;
@@ -42,6 +40,21 @@ int motor_control(h_shell_t* h_shell, int argc, char** argv)
 		size = snprintf(h_shell->print_buffer, SHELL_PRINT_BUFFER_SIZE, "Need 2 arguments : speed ###\r\n");
 		h_shell->drv.transmit(h_shell->print_buffer, size);
 		return HAL_ERROR;
+	}
+
+	// on vérifie que l'argument est bien un nombre
+	char* str = argv[1];
+	int reading_head = 0;
+	while(str[reading_head] != '\0')
+	{
+		char c = str[reading_head];
+		if(!(c >= '0' && c <= '9'))
+		{
+			size = snprintf(h_shell->print_buffer, SHELL_PRINT_BUFFER_SIZE, "2nd arguments isn't number\r\n");
+			h_shell->drv.transmit(h_shell->print_buffer, size);
+			return HAL_ERROR;
+		}
+		reading_head++;
 	}
 
 	speedValue = atoi(argv[1]);
@@ -58,4 +71,82 @@ int motor_control(h_shell_t* h_shell, int argc, char** argv)
 	size = snprintf(h_shell->print_buffer, SHELL_PRINT_BUFFER_SIZE, "Speed %d tr/min ==> DutyCycle %.2f\r\n", speedValue, dutyCycle);
 	h_shell->drv.transmit(h_shell->print_buffer, size);
 	return HAL_ERROR;
+}
+
+
+int motor_dutycycle(h_shell_t* h_shell, int argc, char** argv)
+{
+	int size;
+	int value;
+
+	if(argc!=2){
+		size = snprintf(h_shell->print_buffer, SHELL_PRINT_BUFFER_SIZE, "Need 2 arguments : dutycycle ###\r\n");
+		h_shell->drv.transmit(h_shell->print_buffer, size);
+		return HAL_ERROR;
+	}
+
+	// on vérifie que l'argument est bien un nombre
+	char* str = argv[1];
+	int reading_head = 0;
+	while(str[reading_head] != '\0')
+	{
+		char c = str[reading_head];
+		if(!(c >= '0' && c <= '9'))
+		{
+			size = snprintf(h_shell->print_buffer, SHELL_PRINT_BUFFER_SIZE, "2nd arguments isn't number\r\n");
+			h_shell->drv.transmit(h_shell->print_buffer, size);
+			return HAL_ERROR;
+		}
+		reading_head++;
+	}
+
+	value = atoi(argv[1]);
+
+	if(value > 100 || value < 0){
+		size = snprintf(h_shell->print_buffer, SHELL_PRINT_BUFFER_SIZE, "duty cycle valeur de 0 à 100\r\n");
+		h_shell->drv.transmit(h_shell->print_buffer, size);
+		return HAL_ERROR;
+	}
+	dutyCycle = (float)value/ (float)100.0f;
+	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, (uint32_t)(dutyCycle * PWM_ARR));
+	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, (uint32_t)((1 - dutyCycle) * PWM_ARR));
+
+	size = snprintf(h_shell->print_buffer, SHELL_PRINT_BUFFER_SIZE, "DutyCycle %.2f\r\n", dutyCycle);
+	h_shell->drv.transmit(h_shell->print_buffer, size);
+	return HAL_ERROR;
+}
+
+
+int motor_start(h_shell_t* h_shell, int argc, char** argv)
+{
+	int size;
+
+	if(argc != 1){
+		size = snprintf(h_shell->print_buffer, SHELL_PRINT_BUFFER_SIZE, "usage : motorStart\r\n");
+		h_shell->drv.transmit(h_shell->print_buffer, size);
+		return HAL_ERROR;
+	}
+
+	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
+
+	HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_1);
+	HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_2);
+}
+
+
+int motor_stop(h_shell_t* h_shell, int argc, char** argv)
+{
+	int size;
+
+	if(argc != 1){
+		size = snprintf(h_shell->print_buffer, SHELL_PRINT_BUFFER_SIZE, "usage : motorStop\r\n");
+		h_shell->drv.transmit(h_shell->print_buffer, size);
+		return HAL_ERROR;
+	}
+
+	HAL_TIM_PWM_Stop(&htim1, TIM_CHANNEL_1);
+	HAL_TIM_PWM_Stop(&htim1, TIM_CHANNEL_2);
+	HAL_TIMEx_PWMN_Stop(&htim1, TIM_CHANNEL_1);
+	HAL_TIMEx_PWMN_Stop(&htim1, TIM_CHANNEL_2);
 }
